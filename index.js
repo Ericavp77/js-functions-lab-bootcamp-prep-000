3dcb8143cc99913781bef0f4e6ea7c15478f1fe1
Erica Pitts <ericavp77@gmail.com>
2017-11-03 16:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidays() {
 return "Happy holidays!"
 }
-function happyHolidaysto(name) {
-console.log(`Happy holidays, ${name}!`)
+function happyHolidaysTo(Name) {
+  console.log(`Happy holidays, ${Name}!`)
 }
